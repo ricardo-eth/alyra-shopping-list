965c43a049027704601d7d68524ca4cb36e1f5f8
ricardo-eth <ricardo.soaresdacosta@gmail.com>
2021-04-26 14:04:43 +0200

    Add useEffect and localStorage

--- src/components/ShoppingApp.js
+++ src/components/ShoppingApp.js
@@ -1,42 +1,60 @@
-import {useState} from "react"
-import ShoppingList from "./ShoppingList"
-import AddPopularProducts from "./AddPopularProducts"
-import AddProductForm from "./AddProductForm"
+import { useState, useEffect } from "react";
+import ShoppingList from "./ShoppingList";
+import AddPopularProducts from "./AddPopularProducts";
+import AddProductForm from "./AddProductForm";
 
 const ShoppingApp = () => {
-    const [shopping, setShopping] = useState(["cumin", "curry"])
+  const getInitialShopping = () =>
+    JSON.parse(window.localStorage.getItem("my-shopping-list")) || [];
 
-    const [filter, setFilter] = useState("")
+  const [shopping, setShopping] = useState(getInitialShopping);
 
-    const addToShoppingList = (product) => {
-    setShopping([...shopping, product])
-    setFilter("")
-  }
+  const [filter, setFilter] = useState("");
 
-   const removeFromShoppingList = (product) => {
-    setShopping(shopping.filter(el => el !== product))
-  }
+  const addToShoppingList = (product) => {
+    setShopping([...shopping, product]);
+    setFilter("");
+  };
+
+  const removeFromShoppingList = (product) => {
+    setShopping(shopping.filter((el) => el !== product));
+  };
+
+  useEffect(() => {
+    document.title = shopping.length
+      ? `Vous avez ${shopping.length} produit à acheter`
+      : "Préparez votre liste des courses !";
+  }, [shopping.length]);
+
+  useEffect(() => {
+    window.localStorage.setItem("my-shopping-list", JSON.stringify(shopping));
+  }, [shopping]);
 
   return (
     <main className="row">
       <section className="col-lg-8">
-        <ShoppingList shopping={shopping} removeFromShoppingList={removeFromShoppingList} filter={filter} setFilter={setFilter}/>
+        <ShoppingList
+          shopping={shopping}
+          removeFromShoppingList={removeFromShoppingList}
+          filter={filter}
+          setFilter={setFilter}
+        />
       </section>
       <section className="col-lg-4">
         <div className="bg-light border p-4">
           <h2 className="mb-3 h4">Ajouter un produit :</h2>
-          <AddProductForm 
-          shopping={shopping} 
-          addToShoppingList={addToShoppingList}
+          <AddProductForm
+            shopping={shopping}
+            addToShoppingList={addToShoppingList}
           />
-          <AddPopularProducts 
-          shopping={shopping} 
-          addToShoppingList={addToShoppingList}
+          <AddPopularProducts
+            shopping={shopping}
+            addToShoppingList={addToShoppingList}
           />
         </div>
       </section>
     </main>
-  )
-}
+  );
+};
 
-export default ShoppingApp
+export default ShoppingApp;
